3cbf353ad3fcedb4b73eb40d509bbc4314a14695
Greg Brisebois <gregoryjjb@gmail.com>
2018-12-22 13:57:53 -0800

    Compiled to dist

--- .eslintrc.js
+++ .eslintrc.js
@@ -18,7 +18,7 @@ module.exports = {
     rules: {
         'prettier/prettier': [
             // customizing prettier rules (unfortunately not many of them are customizable)
-            'error',
+            'warn',
             {
                 singleQuote: true,
                 tabWidth: 4,
